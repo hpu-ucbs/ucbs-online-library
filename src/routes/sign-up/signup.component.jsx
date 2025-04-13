14e861d48b5e3e3d400b99b1703c42ffa820a826
divyanshthakurx <divyanshthakurx@gmail.com>
2025-04-13 13:37:07 +0530

    fixed the terms an condiition checkbox

--- src/routes/sign-up/signup.component.jsx
+++ src/routes/sign-up/signup.component.jsx
@@ -64,6 +64,10 @@ const SignUp = () => {
       newErrors.year = "Select Course Year";
     }
 
+    if (!document.getElementById("terms").checked) {
+      newErrors.terms = "You must accept the terms and conditions";
+    }
+  
     return newErrors;
   };
 
@@ -231,7 +235,10 @@ const SignUp = () => {
                       </div>
                       <div className="flex items-start">
                           <div className="flex items-center">
-                            <FormInput id="terms" aria-describedby="terms" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required=""/>
+                            <FormInput id="terms" aria-describedby="terms" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required/>
+                            {errors.terms && (
+                              <p className="text-red-500 pb-1">{errors.terms}</p>
+                            )}
                           </div>
                           <div className="ml-3 text-sm">
                             <label htmlFor="terms" className="font-light text-black dark:text-gray-300">I accept the <Link to="/tas" className="font-semibold text-primary-600 hover:underline dark:text-primary-500 underline">Terms and Conditions</Link></label>
